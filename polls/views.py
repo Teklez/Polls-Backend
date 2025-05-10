16919d363953504ca180e6f87ff46ca21eb796f6
Teklez <zemenumekiria@gmail.com>
2025-05-10 10:29:52 +0300

    fix():polls tamplate loading

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 
 from django.http import HttpResponse
-from django.template import loader
+# from django.template import loader
+from django.shortcuts import render
 from .models import Question
 
 # Create your views here.
@@ -9,8 +10,9 @@ def index(  request):
 
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list":latest_question_list}
-    template = loader.get_template("polls/index.html")
-    return HttpResponse(template.render(context, request))
+    # template = loader.get_template("polls/index.html")
+    # return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
